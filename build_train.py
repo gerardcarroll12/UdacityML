c573bab4628c7b311a91eeb57f213981988cd8d0
gerardcarroll12 <63436113+gerardcarroll12@users.noreply.github.com>
2020-04-10 10:21:19 -0700

    Create build_train.py

---
+++ build_train.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Developing an AI application
+# 
+# Going forward, AI algorithms will be incorporated into more and more everyday applications. For example, you might want to include an image classifier in a smart phone app. To do this, you'd use a deep learning model trained on hundreds of thousands of images as part of the overall application architecture. A large part of software development in the future will be using these types of models as common parts of applications. 
+# 
+# In this project, you'll train an image classifier to recognize different species of flowers. You can imagine using something like this in a phone app that tells you the name of the flower your camera is looking at. In practice you'd train this classifier, then export it for use in your application. We'll be using [this dataset](http://www.robots.ox.ac.uk/~vgg/data/flowers/102/index.html) of 102 flower categories, you can see a few examples below. 
+# 
+# <img src='assets/Flowers.png' width=500px>
+# 
+# The project is broken down into multiple steps:
+# 
+# * Load and preprocess the image dataset
+# * Train the image classifier on your dataset
+# * Use the trained classifier to predict image content
+# 
+# We'll lead you through each part which you'll implement in Python.
+# 
+# When you've completed this project, you'll have an application that can be trained on any set of labeled images. Here your network will be learning about flowers and end up as a command line application. But, what you do with your new skills depends on your imagination and effort in building a dataset. For example, imagine an app where you take a picture of a car, it tells you what the make and model is, then looks up information about it. Go build your own dataset and make something new.
+# 
+# First up is importing the packages you'll need. It's good practice to keep all the imports at the beginning of your code. As you work through this notebook and find you need to import a package, make sure to add the import up here.
+
+# In[1]:
+
+
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+import torchvision.models as models
+
+get_ipython().system('wget https://raw.githubusercontent.com/udacity/deep-learning-v2-pytorch/3bd7dea850e936d8cb44adda8200e4e2b5d627e3/intro-to-pytorch/helper.py')
+
+
+# ## Load the data
+# 
+# Here you'll use `torchvision` to load the data ([documentation](http://pytorch.org/docs/0.3.0/torchvision/index.html)). The data should be included alongside this notebook, otherwise you can [download it here](https://s3.amazonaws.com/content.udacity-data.com/nd089/flower_data.tar.gz). The dataset is split into three parts, training, validation, and testing. For the training, you'll want to apply transformations such as random scaling, cropping, and flipping. This will help the network generalize leading to better performance. You'll also need to make sure the input data is resized to 224x224 pixels as required by the pre-trained networks.
+# 
+# The validation and testing sets are used to measure the model's performance on data it hasn't seen yet. For this you don't want any scaling or rotation transformations, but you'll need to resize then crop the images to the appropriate size.
+# 
+# The pre-trained networks you'll use were trained on the ImageNet dataset where each color channel was normalized separately. For all three sets you'll need to normalize the means and standard deviations of the images to what the network expects. For the means, it's `[0.485, 0.456, 0.406]` and for the standard deviations `[0.229, 0.224, 0.225]`, calculated from the ImageNet images.  These values will shift each color channel to be centered at 0 and range from -1 to 1.
+#  
+
+# In[2]:
+
+
+data_dir = 'flowers'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+
+
+# TODO: Define your transforms for the training, validation, and testing sets
+
+training_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                         transforms.RandomResizedCrop(224),
+                                         transforms.RandomHorizontalFlip(),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406],
+                                                             [0.229, 0.224, 0.225])])
+
+valid_transforms = transforms.Compose([transforms.Resize(255),
+                                           transforms.CenterCrop(224),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.485, 0.456, 0.406],
+                                                             [0.229, 0.224, 0.225])])
+
+testing_transforms = transforms.Compose([transforms.Resize(255),
+                                         transforms.CenterCrop(224),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406],
+                                                             [0.229, 0.224, 0.225])])
+    
+# TODO: Load the datasets with ImageFolder
+
+train_data = datasets.ImageFolder(train_dir, transform=training_transforms)
+valid_data = datasets.ImageFolder(valid_dir, transform=valid_transforms)                                         
+test_data = datasets.ImageFolder(test_dir, transform=testing_transforms)                                       
+                                         
+# TODO: Using the image datasets and the trainforms, define the dataloaders
+                                       
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+validloader = torch.utils.data.DataLoader(valid_data, batch_size=64)                                 
+testloader = torch.utils.data.DataLoader(test_data, batch_size=64)                                     
+
+
+# ### Label mapping
+# 
+# You'll also need to load in a mapping from category label to category name. You can find this in the file `cat_to_name.json`. It's a JSON object which you can read in with the [`json` module](https://docs.python.org/2/library/json.html). This will give you a dictionary mapping the integer encoded categories to the actual names of the flowers.
+
+# In[4]:
+
+
+import json
+
+with open('cat_to_name.json', 'r') as f:
+    cat_to_name = json.load(f)
+
+
+# # Building and training the classifier
+# 
+# Now that the data is ready, it's time to build and train the classifier. As usual, you should use one of the pretrained models from `torchvision.models` to get the image features. Build and train a new feed-forward classifier using those features.
+# 
+# We're going to leave this part up to you. Refer to [the rubric](https://review.udacity.com/#!/rubrics/1663/view) for guidance on successfully completing this section. Things you'll need to do:
+# 
+# * Load a [pre-trained network](http://pytorch.org/docs/master/torchvision/models.html) (If you need a starting point, the VGG networks work great and are straightforward to use)
+# * Define a new, untrained feed-forward network as a classifier, using ReLU activations and dropout
+# * Train the classifier layers using backpropagation using the pre-trained network to get the features
+# * Track the loss and accuracy on the validation set to determine the best hyperparameters
+# 
+# We've left a cell open for you below, but use as many as you need. Our advice is to break the problem up into smaller parts you can run separately. Check that each part is doing what you expect, then move on to the next. You'll likely find that as you work through each part, you'll need to go back and modify your previous code. This is totally normal!
+# 
+# When training make sure you're updating only the weights of the feed-forward network. You should be able to get the validation accuracy above 70% if you build everything right. Make sure to try different hyperparameters (learning rate, units in the classifier, epochs, etc) to find the best model. Save those hyperparameters to use as default values in the next part of the project.
+# 
+# One last important tip if you're using the workspace to run your code: To avoid having your workspace disconnect during the long-running tasks in this notebook, please read in the earlier page in this lesson called Intro to
+# GPU Workspaces about Keeping Your Session Active. You'll want to include code from the workspace_utils.py module.
+# 
+# **Note for Workspace users:** If your network is over 1 GB when saved as a checkpoint, there might be issues with saving backups in your workspace. Typically this happens with wide dense layers after the convolutional layers. If your saved checkpoint is larger than 1 GB (you can open a terminal and check with `ls -lh`), you should reduce the size of your hidden layers and train again.
+
+# In[ ]:
+
+
+# TODO: Build and train your network
+model = models.vgg16(pretrained = True)
+
+
+for param in model.parameters():
+    param.requires_grad = False
+
+from collections import OrderedDict
+classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(25088, 10000)),
+                          ('relu', nn.ReLU()),
+                          ('dropout', nn.Dropout(p=0.3)),
+                          ('fc2', nn.Linear(10000, 5000)),
+                          ('relu', nn.ReLU()),
+                          ('dropout', nn.Dropout(0.3)),
+                          ('fc3', nn.Linear(5000, 1000)),
+                          ('relu', nn.ReLU()),
+                          ('dropout', nn.Dropout(p=0.3)),
+                          ('fc4', nn.Linear(1000, 102)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+    
+model.classifier = classifier
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+criterion = nn.NLLLoss()
+
+optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+
+model.to(device)
+
+epochs = 5
+steps = 0 
+running_loss = 0
+print_every = 5
+
+for epoch in range(epochs):
+    for images, labels in trainloader:
+        steps += 1
+        
+        images, labels = images.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        
+        logps = model(images)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            model.eval()
+            test_loss = 0
+            accuracy = 0
+            
+            for images, labels in validloader:
+                images, labels = images.to(device), labels.to(device)
+                logps = model(images)
+                loss = criterion(logps, labels)
+                test_loss += loss.item()
+                    
+                ps = torch.exp(logps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equality = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equality.type(torch.FloatTensor)).item()
+                    
+            print(f"Epoch {epoch+1}/{epochs}.. "
+                  f"Train loss: {running_loss/print_every:.3f}.. "
+                  f"Test loss: {test_loss/len(validloader):.3f}.. "
+                  f"Test accuracy: {accuracy/len(validloader):.3f}")
+            running_loss = 0
+
+
+# ## Testing your network
+# 
+# It's good practice to test your trained network on test data, images the network has never seen either in training or validation. This will give you a good estimate for the model's performance on completely new images. Run the test images through the network and measure the accuracy, the same way you did validation. You should be able to reach around 70% accuracy on the test set if the model has been trained well.
+
+# In[ ]:
+
+
+# TODO: Do validation on the test set
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import helper
+images, labels = next(iter(testloader))
+
+img = images[0].view(1, 150528)
+
+with torch.no_grad():
+    logps = model.forward(img)
+
+
+ps = torch.exp(logps)
+helper.view_classify(img.view(1, 25088, 25088), ps)
