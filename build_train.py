084a1dac6146511927957e4ad2ada73f97f5ee71
gerardcarroll12 <63436113+gerardcarroll12@users.noreply.github.com>
2020-04-20 01:05:54 -0700

    Update build_train.py

--- build_train.py
+++ build_train.py
@@ -24,12 +24,19 @@
 # In[1]:
 
 
+# Imports here
 import torch
 from torch import nn
 from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
+from collections import OrderedDict
 import torchvision.models as models
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
 
 get_ipython().system('wget https://raw.githubusercontent.com/udacity/deep-learning-v2-pytorch/3bd7dea850e936d8cb44adda8200e4e2b5d627e3/intro-to-pytorch/helper.py')
 
@@ -52,8 +59,10 @@ valid_dir = data_dir + '/valid'
 test_dir = data_dir + '/test'
 
 
-# TODO: Define your transforms for the training, validation, and testing sets
+# In[3]:
 
+
+# TODO: Define your transforms for the training, validation, and testing sets
 training_transforms = transforms.Compose([transforms.RandomRotation(30),
                                          transforms.RandomResizedCrop(224),
                                          transforms.RandomHorizontalFlip(),
@@ -72,18 +81,19 @@ testing_transforms = transforms.Compose([transforms.Resize(255),
                                          transforms.ToTensor(),
                                          transforms.Normalize([0.485, 0.456, 0.406],
                                                              [0.229, 0.224, 0.225])])
-    
-# TODO: Load the datasets with ImageFolder
 
+
+# TODO: Load the datasets with ImageFolder
 train_data = datasets.ImageFolder(train_dir, transform=training_transforms)
 valid_data = datasets.ImageFolder(valid_dir, transform=valid_transforms)                                         
-test_data = datasets.ImageFolder(test_dir, transform=testing_transforms)                                       
-                                         
+test_data = datasets.ImageFolder(test_dir, transform=testing_transforms) 
+
 # TODO: Using the image datasets and the trainforms, define the dataloaders
-                                       
 trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 validloader = torch.utils.data.DataLoader(valid_data, batch_size=64)                                 
-testloader = torch.utils.data.DataLoader(test_data, batch_size=64)                                     
+testloader = torch.utils.data.DataLoader(test_data, batch_size=64)          
+
+image_datasets = [train_data, valid_data, test_data]
 
 
 # ### Label mapping
@@ -119,11 +129,11 @@ with open('cat_to_name.json', 'r') as f:
 # 
 # **Note for Workspace users:** If your network is over 1 GB when saved as a checkpoint, there might be issues with saving backups in your workspace. Typically this happens with wide dense layers after the convolutional layers. If your saved checkpoint is larger than 1 GB (you can open a terminal and check with `ls -lh`), you should reduce the size of your hidden layers and train again.
 
-# In[ ]:
+# In[5]:
 
 
 # TODO: Build and train your network
-model = models.vgg16(pretrained = True)
+model = models.vgg11(pretrained = True)
 
 
 for param in model.parameters():
@@ -134,12 +144,9 @@ classifier = nn.Sequential(OrderedDict([
                           ('fc1', nn.Linear(25088, 10000)),
                           ('relu', nn.ReLU()),
                           ('dropout', nn.Dropout(p=0.3)),
-                          ('fc2', nn.Linear(10000, 5000)),
+                          ('fc2', nn.Linear(10000, 1000)),
                           ('relu', nn.ReLU()),
                           ('dropout', nn.Dropout(0.3)),
-                          ('fc3', nn.Linear(5000, 1000)),
-                          ('relu', nn.ReLU()),
-                          ('dropout', nn.Dropout(p=0.3)),
                           ('fc4', nn.Linear(1000, 102)),
                           ('output', nn.LogSoftmax(dim=1))
                           ]))
@@ -154,7 +161,7 @@ optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
 
 model.to(device)
 
-epochs = 5
+epochs = 1
 steps = 0 
 running_loss = 0
 print_every = 5
@@ -195,26 +202,186 @@ for epoch in range(epochs):
                   f"Test loss: {test_loss/len(validloader):.3f}.. "
                   f"Test accuracy: {accuracy/len(validloader):.3f}")
             running_loss = 0
+model.train()
 
 
 # ## Testing your network
 # 
 # It's good practice to test your trained network on test data, images the network has never seen either in training or validation. This will give you a good estimate for the model's performance on completely new images. Run the test images through the network and measure the accuracy, the same way you did validation. You should be able to reach around 70% accuracy on the test set if the model has been trained well.
 
-# In[ ]:
+# In[7]:
 
 
 # TODO: Do validation on the test set
+model.eval()
+test_loss = 0
+accuracy = 0
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-import helper
-images, labels = next(iter(testloader))
+for images, labels in testloader:
+    images, labels = images.to(device), labels.to(device)
+    logps = model(images)
+    loss = criterion(logps, labels)
+    test_loss += loss.item()
+    
+    ps = torch.exp(logps)
+    top_p, top_class = ps.topk(1, dim = 1)
+    equality = top_class == labels.view(top_class.shape)
+    accuracy += torch.mean(equality.type(torch.FloatTensor)).item()
+    
+    print(f"Train loss: {running_loss/print_every:.3f}.. "
+          f"Test loss: {test_loss/len(validloader):.3f}.. "
+          f"Test accuracy: {accuracy/len(validloader):.3f}")
+    running_loss = 0
 
-img = images[0].view(1, 150528)
 
-with torch.no_grad():
-    logps = model.forward(img)
+# ## Save the checkpoint
+# 
+# Now that your network is trained, save the model so you can load it later for making predictions. You probably want to save other things such as the mapping of classes to indices which you get from one of the image datasets: `image_datasets['train'].class_to_idx`. You can attach this to the model as an attribute which makes inference easier later on.
+# 
+# ```model.class_to_idx = image_datasets['train'].class_to_idx```
+# 
+# Remember that you'll want to completely rebuild the model later so you can use it for inference. Make sure to include any information you need in the checkpoint. If you want to load the model and keep training, you'll want to save the number of epochs as well as the optimizer state, `optimizer.state_dict`. You'll likely want to use this trained model in the next part of the project, so best to save it now.
+
+# In[8]:
 
 
-ps = torch.exp(logps)
-helper.view_classify(img.view(1, 25088, 25088), ps)
+# TODO: Save the checkpoint
+checkpoint = {'state_dict': model.classifier.state_dict(),
+              'class_to_idx': image_datasets[0].class_to_idx,
+              'epochs': 1,
+              'optimizer.state_dict': optimizer.state_dict}
+
+torch.save(checkpoint, 'checkpoint.pth')
+
+
+# ## Loading the checkpoint
+# 
+# At this point it's good to write a function that can load a checkpoint and rebuild the model. That way you can come back to this project and keep working on it without having to retrain the network.
+
+# In[9]:
+
+
+# TODO: Write a function that loads a checkpoint and rebuilds the model
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model.load_state_dict(checkpoint['state_dict'])
+    model.class_to_idx = checkpoint['class_to_idx']
+    model.optimizer.load_state_dict = checkpoint['optimizer.state_dict']
+    load_checkpoint('checkpoint.pth')
+
+
+# # Inference for classification
+# 
+# Now you'll write a function to use a trained network for inference. That is, you'll pass an image into the network and predict the class of the flower in the image. Write a function called `predict` that takes an image and a model, then returns the top $K$ most likely classes along with the probabilities. It should look like 
+# 
+# ```python
+# probs, classes = predict(image_path, model)
+# print(probs)
+# print(classes)
+# > [ 0.01558163  0.01541934  0.01452626  0.01443549  0.01407339]
+# > ['70', '3', '45', '62', '55']
+# ```
+# 
+# First you'll need to handle processing the input image such that it can be used in your network. 
+# 
+# ## Image Preprocessing
+# 
+# You'll want to use `PIL` to load the image ([documentation](https://pillow.readthedocs.io/en/latest/reference/Image.html)). It's best to write a function that preprocesses the image so it can be used as input for the model. This function should process the images in the same manner used for training. 
+# 
+# First, resize the images where the shortest side is 256 pixels, keeping the aspect ratio. This can be done with the [`thumbnail`](http://pillow.readthedocs.io/en/3.1.x/reference/Image.html#PIL.Image.Image.thumbnail) or [`resize`](http://pillow.readthedocs.io/en/3.1.x/reference/Image.html#PIL.Image.Image.thumbnail) methods. Then you'll need to crop out the center 224x224 portion of the image.
+# 
+# Color channels of images are typically encoded as integers 0-255, but the model expected floats 0-1. You'll need to convert the values. It's easiest with a Numpy array, which you can get from a PIL image like so `np_image = np.array(pil_image)`.
+# 
+# As before, the network expects the images to be normalized in a specific way. For the means, it's `[0.485, 0.456, 0.406]` and for the standard deviations `[0.229, 0.224, 0.225]`. You'll want to subtract the means from each color channel, then divide by the standard deviation. 
+# 
+# And finally, PyTorch expects the color channel to be the first dimension but it's the third dimension in the PIL image and Numpy array. You can reorder dimensions using [`ndarray.transpose`](https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.ndarray.transpose.html). The color channel needs to be first and retain the order of the other two dimensions.
+
+# In[16]:
+
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    
+    # TODO: Process a PIL image for use in a PyTorch model
+    
+    pil_image = Image.open(image)
+    pil_image = pil_image.resize(256,256)
+    pil_image = pil_image.crop(16,16,240,240)
+    pil_img = np.array(img)/255
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    pil_img = (pil_img - mean)/std
+    pil_img = pil_img.transpose((2, 0, 1))
+    
+    return pil_img
+
+
+# To check your work, the function below converts a PyTorch tensor and displays it in the notebook. If your `process_image` function works, running the output through this function should return the original image (except for the cropped out portions).
+
+# In[17]:
+
+
+def imshow(image, ax=None, title=None):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    pil_image = pil_image.numpy().transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    pil_image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    pil_image = np.clip(image, 0, 1)
+    
+    ax.imshow(pil_image)
+    
+    return ax
+
+
+# ## Class Prediction
+# 
+# Once you can get images in the correct format, it's time to write a function for making predictions with your model. A common practice is to predict the top 5 or so (usually called top-$K$) most probable classes. You'll want to calculate the class probabilities then find the $K$ largest values.
+# 
+# To get the top $K$ largest values in a tensor use [`x.topk(k)`](http://pytorch.org/docs/master/torch.html#torch.topk). This method returns both the highest `k` probabilities and the indices of those probabilities corresponding to the classes. You need to convert from these indices to the actual class labels using `class_to_idx` which hopefully you added to the model or from an `ImageFolder` you used to load the data ([see here](#Save-the-checkpoint)). Make sure to invert the dictionary so you get a mapping from index to class as well.
+# 
+# Again, this method should take a path to an image and a model checkpoint, then return the probabilities and classes.
+# 
+# ```python
+# probs, classes = predict(image_path, model)
+# print(probs)
+# print(classes)
+# > [ 0.01558163  0.01541934  0.01452626  0.01443549  0.01407339]
+# > ['70', '3', '45', '62', '55']
+# ```
+
+# In[ ]:
+
+
+def predict(image_path, model, topk=5):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    
+    # TODO: Implement the code to predict the class from an image file
+    
+
+
+# ## Sanity Checking
+# 
+# Now that you can use a trained model for predictions, check to make sure it makes sense. Even if the testing accuracy is high, it's always good to check that there aren't obvious bugs. Use `matplotlib` to plot the probabilities for the top 5 classes as a bar graph, along with the input image. It should look like this:
+# 
+# <img src='assets/inference_example.png' width=300px>
+# 
+# You can convert from the class integer encoding to actual flower names with the `cat_to_name.json` file (should have been loaded earlier in the notebook). To show a PyTorch tensor as an image, use the `imshow` function defined above.
+
+# In[ ]:
+
+
+# TODO: Display an image along with the top 5 classes
+
